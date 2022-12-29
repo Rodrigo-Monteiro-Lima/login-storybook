eb9d31cddd40af171d19f9ddc5c503d25482cfee
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-29 17:22:21 -0300

    test: login sucesss

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
 import SignIn from './SignIn'
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 export default {
   title: 'Pages/Sign In',
@@ -8,4 +10,13 @@ export default {
   argTypes: {}
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj = {
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement)
+    userEvent.type(canvas.getByPlaceholderText('Digite seu e-mail'), 'rodrigo@test.com.br');
+    userEvent.type(canvas.getByPlaceholderText('******'), '123456789');
+    userEvent.click(canvas.getByRole('button'));
+    await waitFor(() => expect(canvas.getByText('Login realizado!')).toBeInTheDocument()); 
+  }
+
+}
